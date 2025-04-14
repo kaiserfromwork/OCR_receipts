d04053de6da54bef851f34c7f9d9ba57b5ce7975
kaisefromwork <kaiserfromwork@gmail.com>
2025-04-14 12:44:07 -0300

    created .toml and .yml files for GitHub actions

--- src/logging_config.py
+++ src/logging_config.py
@@ -1,11 +1,15 @@
 import logging
 
 
-logger = logging.getLogger("Logger")
-logger.setLevel(logging.DEBUG)
+def main() -> None:
+    logger = logging.getLogger("Logger")
+    logger.setLevel(logging.DEBUG)
 
-file_handler = logging.FileHandler("logging_file.log")
-file_handler.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler("logging_file.log")
+    file_handler.setLevel(logging.DEBUG)
 
+
+if __name__ == "__main__":
+    main()
 
 
